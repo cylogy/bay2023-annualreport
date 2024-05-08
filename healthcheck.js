dc553c7608f1b38e1f26106f442a2be093fbf0ee
Charly Palencia <charly@guarapo.dev>
2024-05-08 07:46:10 -0500

    Implement nextjs dockerfile

---
+++ healthcheck.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var options = {
+  host: "localhost",
+  port: process.env.PORT || 3000,
+  timeout: 30000,
+};
+var request = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  if (res.statusCode == 200) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+});
+request.on("error", function (err) {
+  console.log("ERROR");
+  process.exit(1);
+});
+request.end();
