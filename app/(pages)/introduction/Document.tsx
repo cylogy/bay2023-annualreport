83955e7ae7c363e03a76c9c20e8fd63e0d23378a
Juan Insignares <juan.insignares@guarapo.dev>
2024-05-15 22:14:51 +0200

    fixed image

--- app/(pages)/introduction/Document.tsx
+++ app/(pages)/introduction/Document.tsx
@@ -79,7 +79,7 @@ const Document = (props: Props) => {
               className="relative flex h-full w-full flex-col items-center justify-center"
             >
               <Image
-                src="/img/rectangle-251.png"
+                src="/img/rectangle-251.jpg"
                 fill
                 alt="Two hikers on a mountain trail"
                 className="object-cover "
